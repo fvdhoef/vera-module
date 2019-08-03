99e6160778598c90002639d31a39ed7ccd36de21
Frank van den Hoef <frank@vandenhoef.nl>
2019-08-03 20:09:39 +0200

    Implemented font inversion in logic instead of ROM to save memory.

--- fpga/source/char_rom.v
+++ fpga/source/char_rom.v
@@ -4,13 +4,19 @@ module char_rom(
     input  wire        clk,
 
     input  wire  [9:0] rd_addr,
-    output reg  [31:0] rd_data);
+    output wire [31:0] rd_data);
 
-    reg [31:0] mem_r[0:1023];
+    wire [8:0] mem_addr = {rd_addr[9], rd_addr[7:0]};
 
-    always @(posedge clk) begin
-        rd_data <= mem_r[rd_addr];
-    end
+    reg [31:0] mem_r[0:511];
+
+    reg invert_r;
+    always @(posedge clk) invert_r <= rd_addr[8];
+
+    reg [31:0] data_r;
+    always @(posedge clk) data_r <= mem_r[mem_addr];
+
+    assign rd_data = invert_r ? ~data_r : data_r;
 
     initial begin
         `ifdef __ICARUS__
